fbc9f42993597e7a5efbdbf9a3e5d7ac63a01812
Joao Antonio <134729023+JoaoAntonioTDS@users.noreply.github.com>
2025-06-05 14:57:55 -0300

    Update CriaClasses4.php

--- CriarClasses/CriaClasses4.php
+++ CriarClasses/CriaClasses4.php
@@ -11,34 +11,52 @@ class CriaClasses1 {
     function ClassesModel() {
         if (!file_exists("sistema")) {
             mkdir("sistema");
-        if (!file_exists("sistema/model"))
-              mkdir("sistema/model");
+            if (!file_exists("sistema/model"))
+                mkdir("sistema/model");
         }
+
         $sql = "SHOW TABLES";
         $query = $this->con->query($sql);
         $tabelas = $query->fetchAll(PDO::FETCH_ASSOC);
 
         foreach ($tabelas as $tabela) {
             $nomeTabela = array_values((array) $tabela)[0];
-            $sql="show columns from ".$nomeTabela;
+            $sql = "SHOW COLUMNS FROM " . $nomeTabela;
             $atributos = $this->con->query($sql)->fetchAll(PDO::FETCH_OBJ);
-            $nomeAtributos="";
-            foreach ($atributos as $atributo) {
-                $nomeAtributos.="private \${$atributo->Field};\n";
 
+            $nomeAtributos = "";
+            $getSet = "";
+
+            foreach ($atributos as $atributo) {
+                $campo = $atributo->Field;
+                $camelCampo = ucfirst($campo);
+
+                $nomeAtributos .= "    private \${$campo};\n";
+
+                $getSet .= "    public function get{$camelCampo}() {\n";
+                $getSet .= "        return \$this->{$campo};\n";
+                $getSet .= "    }\n\n";
+
+                $getSet .= "    public function set{$camelCampo}(\${$campo}) {\n";
+                $getSet .= "        \$this->{$campo} = \${$campo};\n";
+                $getSet .= "    }\n\n";
             }
-            $nomeTabela=ucfirst($nomeTabela);
+
+            $nomeTabela = ucfirst($nomeTabela);
+
             $conteudo = <<<EOT
 <?php
 class {$nomeTabela} {
 {$nomeAtributos}
-}
+
+{$getSet}}
 ?>
 EOT;
-      file_put_contents("sistema/model/{$nomeTabela}.php", $conteudo);
 
+            file_put_contents("sistema/model/{$nomeTabela}.php", $conteudo);
         }
     }
 }
 
 (new CriaClasses1())->ClassesModel();
+
